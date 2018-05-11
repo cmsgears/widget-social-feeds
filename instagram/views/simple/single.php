0ecc7961a351567246b03aaa6c5c06e20af4e4dd
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-05-11 07:21:20 +0530

    Applied quick fixes.

--- instagram/views/simple/single.php
+++
@@ -1,3 +0,0 @@
-<a href="<?= $post[ 'url' ] ?>" class="col col12x6 post">
-	<img class="fluid grow" src="<?= $post[ 'img' ] ?>">
-</a>
--- instagram/views/simple/wrapper.php
+++
@@ -1,4 +0,0 @@
-<?php if( !empty( $widget->title ) ) { ?>
-<h4><?= $widget->title ?></h4>
-<?php } ?>
-<?= $postsHtml ?>
